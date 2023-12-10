650ae3b8e47ec47a86136ed2c75204cf6fd59127
Victor Alcaraz <vctralcaraz@gmail.com>
2023-12-10 12:27:38 -0800

    all phases complete

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,73 @@
 export function getAllDogs() {
     // Your code here
+    const url = '/dogs';
+
+    return fetch(url);
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    const url = '/dogs/2';
+
+    return fetch(url);
 }
 
 export function postNewDog() {
     // Your code here
+    const url = '/dogs';
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const body = new URLSearchParams({
+        name: 'name',
+        age: 5
+    });
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        body: body
+    }
+
+    return fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
      // Your code here
+     const url = '/dogs';
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const body = new URLSearchParams({
+        name: name,
+        age: age
+    });
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        body: body
+    }
+
+    return fetch(url, options);
 }
 
 export function deleteDog(id) {
       // Your code here
+      const url = `/dogs/${id}/delete`;
+
+      const headers = {
+        'AUTH': 'ckyut5wau0000jyv5bsrud90y'
+      }
+
+      const options = {
+        method: 'POST',
+        headers: headers
+      }
+
+      return fetch(url, options);
 }
